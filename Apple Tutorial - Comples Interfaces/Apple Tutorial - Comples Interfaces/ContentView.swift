5a069726a2cfe71cdabc4df46219128da35b5e1e
my-les <mcutchember@gmail.com>
2020-08-06 13:35:01 -0700

    done.

---
+++ Apple Tutorial - Comples Interfaces/Apple Tutorial - Comples Interfaces/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apple Tutorial - Comples Interfaces
+//
+//  Created by Myle$ on 8/4/20.
+//  Copyright © 2020 Myle$. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
